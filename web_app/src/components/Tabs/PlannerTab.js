45be448c9f51de2bfdc6734a4cfa5426c81a52cd
Tom Walsh <tomwalsh96@gmail.com>
2021-06-21 15:36:44 +0100

    React App Initial Basic Structure
    - Added some Tabs with filler content, using react-tabs module
    - No tests have been created yet
    - Limited styling

---
+++ web_app/src/components/Tabs/PlannerTab.js
@@ -0,0 +1,7 @@
+function PlannerTab() {
+  return(
+    <h3>Journey Planner Goes Here</h3>
+  )
+}
+
+export default PlannerTab;
---
+++ web_app/src/components/Tabs/RealtimeTab.js
@@ -0,0 +1,7 @@
+function RealtimeTab() {
+  return(
+    <h3>Realtime Info Goes Here</h3>
+  )
+}
+
+export default RealtimeTab;
