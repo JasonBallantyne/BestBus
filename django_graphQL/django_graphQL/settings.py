cbe327928440c6a68ec3ab3dbddf36b0c78ae496
Jonathan Farrell <jonathan.farrell11@gmail.com>
2021-06-25 12:16:58 +0100

    tidy up of django directory and change of database settings for temporary testing

--- django_graphQL/django_graphQL/settings.py
+++ django_graphQL/django_graphQL/settings.py
@@ -66,15 +66,10 @@ WSGI_APPLICATION = 'django_graphQL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'team3',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'team3',
-        'PASSWORD': '7Cf6p4MRRJYS4v9b',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
