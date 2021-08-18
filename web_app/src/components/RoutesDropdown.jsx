41b89625bf738da37d03184830a4dc1a8c84a8d0
Tom Walsh <tomwalsh96@gmail.com>
2021-08-18 21:10:09 +0100

    Feature/favourites (#25) 🗒
    * changed favicon to bus logo
    
    * simple user favourites implementation
    
    * added functionality for stop favourites
    
    - also coloured favourites grey
    
    * bug fix, showing too many buttons on stops

--- web_app/src/components/RoutesDropdown.jsx
+++ web_app/src/components/RoutesDropdown.jsx
@@ -1,6 +1,8 @@
 import { useQuery, gql } from "@apollo/client";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { StationsContext } from "../contexts/stations";
+import CloseButton from 'react-bootstrap/CloseButton';
+import { MdDelete } from "react-icons/md";
 
 const ROUTES = gql`
   query {
@@ -24,9 +26,67 @@ function  RoutesDropdown() {
   const { loading, error, data } = useQuery(ROUTES);
   const [routeSearch, setRouteSearch] = useState('');
 
+  const [favourites, setFavourites] = useState([]);
+  const [updateFavourites, setUpdateFavourites] = useState(false);
+
+  useEffect(() => {
+    setFavourites(getRouteFavourites())
+    setUpdateFavourites(false)
+  }, [updateFavourites])
+
+  // function setFavourites(favs) {
+  //   localStorage.setItem('favourites', JSON.stringify(favs));
+  // }
+
+  // fucntion to get the route favourites from local storage
+  function getRouteFavourites() {
+    return JSON.parse(localStorage.getItem('route-favourites') || '[]')
+  };
+
+  // function adds route to favourites array in local storage, removes oldest if array larger than 3
+  function addToRouteFavourites(item) {
+    let favourites = JSON.parse(localStorage.getItem('route-favourites') || '[]');
+    let include = true;
+    favourites.forEach((i) => {
+      if (i.id === item.id) {
+        include = false;
+      }
+    })
+    if (include) {
+      favourites.push(item)
+    }
+    while (favourites.length > 3) {
+      favourites.shift()
+    }
+    localStorage.setItem('route-favourites', JSON.stringify(favourites));
+    setUpdateFavourites(true)
+  };
+
+  // function to remove a given route from favourites in local storage
+  function removeFromRouteFavourites(item) {
+    let favourites = JSON.parse(localStorage.getItem('route-favourites') || '[]');
+    let counter = 0;
+    let index = 0;
+    favourites.forEach((i) => {
+      if (i.id === item.id) {
+        index = counter;
+      }
+      counter++;
+    })
+    if (index < favourites.length) {
+      favourites.splice(index, 1);
+    }
+    localStorage.setItem('route-favourites', JSON.stringify(favourites));
+    setUpdateFavourites(true)
+  };
+
   const [, dispatch ] = useContext(StationsContext);
 
+  // this function is fired each time a route button is selected
   function chooseRoute(route) {
+
+    addToRouteFavourites(route)
+
     // get all required gtfs_data split out
     let stopNums = route.stops.split(",");
     let stopNames = route.names.split(",");
@@ -43,18 +103,35 @@ function  RoutesDropdown() {
       "longitude": longitudes[i].trim(), "latitude": latitudes[i].trim(), "stopName": stopNames[i].trim(), "stopNum": stopNums[i].trim(),
       "irishName": irishNames[i].trim(), "departureSchedule": route.firstDepartureSchedule};
       routeOrganised.push(newStop)
-      // console.log(newStop)
     }
+
+    // console.log(routeOrganised)
+
     dispatch({type: "update_stations", payload: [routeOrganised]})
   }
 
   const container = {
     width: "13vw",
     minWidth: "11rem",
-  }
+  };
   const buttonContainer = {
     height: "10rem"
   };
+  const favouriteDiv = {
+    display: "grid",
+    gridTemplateColumns: "5fr 1fr"
+  };
+  const favouritesButton = {
+    display: "block",
+    width: "100%",
+    height: "2rem",
+    margin: "3% 0",
+    backgroundColor: "grey"
+  };
+  const removeButton = {
+    height: "2rem",
+    margin: "14% 0"
+  }
   const button = {
     display: "block",
     width: "100%",
@@ -70,6 +147,11 @@ function  RoutesDropdown() {
       <h3>Choose a Route</h3>
       <input type="text" placeholder="Search by route number" onChange={event => {setRouteSearch(event.target.value)}} />
       <div style={buttonContainer}>
+        {favourites.map((route) => {
+          return (
+            <div style={favouriteDiv} key={"favdiv-" + route.lineId + "-" + route.direction}><input type="button" style={favouritesButton} key={"favinput-" + route.lineId + "-" + route.direction} value={route.lineId + " (" + route.direction + ")"} onClick={ () => {chooseRoute(route)}}></input><CloseButton onClick={() => {removeFromRouteFavourites(route)}} style={removeButton}><MdDelete/></CloseButton></div>
+          )
+        })}
         { data.uniqueRoutes.filter((val)=> {
           if (routeSearch === "") {
             return val
@@ -78,7 +160,7 @@ function  RoutesDropdown() {
           } else {
             return null
           }
-        }).slice(0, 8).map((route) => {
+        }).slice(0, 8-favourites.length).map((route) => {
           return (
             <input type="button" style={button} key={route.lineId + "-" + route.direction} value={route.lineId + " (" + route.direction + ")"} onClick={ () => {chooseRoute(route)}}></input>
           )
--- web_app/src/components/StopsDropdown.jsx
+++ web_app/src/components/StopsDropdown.jsx
@@ -1,6 +1,8 @@
 import { useQuery, gql } from "@apollo/client";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { StationsContext } from "../contexts/stations";
+import CloseButton from 'react-bootstrap/CloseButton';
+import { MdDelete } from "react-icons/md";
 
 const STOPS = gql`
   query {
@@ -20,18 +22,84 @@ function  StopsDropdown() {
 
   const [, dispatch ] = useContext(StationsContext);
 
+  const [favourites, setFavourites] = useState([]);
+  const [updateFavourites, setUpdateFavourites] = useState(false);
+
+  useEffect(() => {
+    setFavourites(getStopFavourites())
+    setUpdateFavourites(false)
+  }, [updateFavourites])
+
+  // fucntion to get the stop favourites from local storage
+  function getStopFavourites() {
+    return JSON.parse(localStorage.getItem('stop-favourites') || '[]')
+  };
+
+  // function adds stop to favourites array in local storage, removes oldest if array larger than 3
+  function addToStopFavourites(item) {
+    let favourites = getStopFavourites();
+    let include = true;
+    favourites.forEach((i) => {
+      if (i.stopNum === item.stopNum) {
+        include = false;
+      }
+    })
+    if (include) {
+      favourites.push(item)
+    }
+    while (favourites.length > 3) {
+      favourites.shift()
+    }
+    localStorage.setItem('stop-favourites', JSON.stringify(favourites));
+    setUpdateFavourites(true)
+  };
+
+  // function to remove a given stop from favourites in local storage
+  function removeFromStopFavourites(item) {
+    let favourites = JSON.parse(localStorage.getItem('stop-favourites') || '[]');
+    let counter = 0;
+    let index = 0;
+    favourites.forEach((i) => {
+      if (i.stopNum === item.stopNum) {
+        index = counter;
+      }
+      counter++;
+    })
+    if (index < favourites.length) {
+      favourites.splice(index, 1);
+    }
+    localStorage.setItem('stop-favourites', JSON.stringify(favourites));
+    setUpdateFavourites(true)
+  };
+
+  // this function is fired each time a stop button is selected
   function chooseStop(stop) {
+    addToStopFavourites(stop);
     dispatch({type: "update_stations", payload: [stop]})
   };
 
   const container = {
     width: "13vw",
     minWidth: "11rem",
-    height: "400px"
   };
   const buttonContainer = {
     height: "10rem"
   };
+  const favouriteDiv = {
+    display: "grid",
+    gridTemplateColumns: "5fr 1fr"
+  };
+  const favouritesButton = {
+    display: "block",
+    width: "100%",
+    height: "2rem",
+    margin: "3% 0",
+    backgroundColor: "grey"
+  };
+  const removeButton = {
+    height: "2rem",
+    margin: "14% 0"
+  }
   const button = {
     display: "block",
     width: "100%",
@@ -47,6 +115,11 @@ function  StopsDropdown() {
       <h3>Choose a Stop</h3>
       <input type="text" placeholder="Search by stop number" onChange={event => {setStopSearch(event.target.value)}} />
       <div style={buttonContainer}>
+        {favourites.map((stop) => {
+          return (
+            <div style={favouriteDiv} key={"favdiv-" + stop.stopNum}><input type="button" style={favouritesButton} key={"favinput-" + stop.stopNum} value={stop.stopNum} onClick={ () => {chooseStop(stop)}}></input><CloseButton onClick={() => {removeFromStopFavourites(stop)}} style={removeButton}><MdDelete/></CloseButton></div>
+          )
+        })}
         { data.uniqueStops.filter((val)=> {
           if (stopSearch === "") {
             return val
@@ -55,7 +128,7 @@ function  StopsDropdown() {
           } else {
             return null
           }
-        }).slice(0, 8).map((stop) => {
+        }).slice(0, 8-favourites.length).map((stop) => {
           return (
             <input type="button" style={button} key={stop.stopNum} value={stop.stopNum} onClick={ () => {chooseStop(stop)}}></input>
           )
